b6935ea4cf62d6c1d666f08e272eeadcd7009f58
Phyks (Lucas Verney) <webmaster@phyks.me>
2013-11-23 19:29:11 +0100

    Refactor du code + un peu de doc

--- ATmega/ATmega.ino
+++ ATmega/ATmega.ino
@@ -1,100 +1,45 @@
-/*
-Pins :
-======
-15 : Led R
-16 : Led G
-17 : Led B
-
-Paquets serial :
-================
-[synchro = 1]
-[fonction] => 2 bits
-[compteur] => 5 bits
-    ===
-[synchro = 0]
-[couleurR] => 7 bits
-    ===
-[synchro = 0]
-[couleurG] => 7 bits
-    ===
-[synchro = 0]
-[couleur B] => 7 bits
-
-Fonctions :
-===========
-0 => Switch immédiat
-1 => Fade in
-2 => Broadcast => applique le paquet et le forward quand même
-3 => Automatique
-*/
-
 #define R 0
 #define G 1
 #define B 2
 const int pins[3] = {15, 16, 17};
 
-#define SERIAL_SPEED 19200
-#define DEBUG 0
-
-int duty[3] = {0, 0, 0};
+#define SERIAL_SPEED 115200
 
 int serial_i = -1;
 byte incoming_byte;
 
 void setup() {
-  Serial.begin(SERIAL_SPEED);
+    Serial.begin(SERIAL_SPEED);
 }
 
 void loop() {
-  // Note : no need for pinMode before analogWrite
-  analogWrite(pins[R], duty[R]);
-  analogWrite(pins[G], duty[G]);
-  analogWrite(pins[B], duty[B]);
-  
-  if(DEBUG) {
-    Serial.print("R : ");
-    Serial.println(duty[R]);
-    Serial.print("G : ");
-    Serial.println(duty[G]);
-    Serial.print("B : ");
-    Serial.println(duty[B]);
-  }
-}
+    if(Serial.available()) {
+        incoming_byte = (byte) Serial.read();
 
-void serialEvent() {
-  //serialEvent est appelée à la fin de la boucle si des données sont dispos sur le RX
-  while(Serial.available()) {
-    incoming_byte = (byte) Serial.read();
-    
-    if(highByte(incoming_byte)) {
-      // Header
-      serial_i = incoming_byte & B00011111; // serial_i = compteur
-      // Note: serial_i = 0 => à traiter
-      
-      // Note : [Unused] serial_fonction = (incoming_byte >> 5) & B011;
-      
-      if(serial_i != 0) {
-        // Forward
-        Serial.print((incoming_byte & B11100000) | (serial_i - 1)); // Décrément du compteur
-        serial_i = -1
-      }
-      
-      if(incoming_byte & B01100000 == B01000000) {
-        // Broadcast
-        Serial.print(incoming_byte & B11100000); // Forward avec compteur nul
-      }
+        // Header
+        if(highByte(incoming_byte)) {
+            serial_i = incoming_byte & B00111111;
+
+            // Forward avec décrément du compteur
+            if(serial_i != 0) {
+                Serial.print((incoming_byte & B11000000) | (serial_i - 1));
+                serial_i = -1
+            }
+
+            // Broadcast
+            if(incoming_byte & B01000000 == B01000000) {
+                Serial.print(incoming_byte & B11000000);
+            }
+        }
+        // Paquet de couleur
+        else {
+            if(serial_i != -1) {
+                analogWrite(pins[serial_i], incoming_byte << 1);
+                serial_i++;
+            }
+            else {
+                Serial.print(incoming_byte);
+            }
+        }
     }
-    else {
-      // Paquet de couleur
-      if(serial_i != -1) {
-        // Si on doit traiter le paquet, on le fait
-        duty[serial_i] = incoming_byte << 1;
-        serial_i++;
-      }
-      else {
-        // Sinon, on forward tel quel
-        Serial.print(incoming_byte);
-      }
-    }
-  }
 }
---
+++ ATmega/README.md
@@ -0,0 +1,50 @@
+Code sur les ATmega
+====
+
+Quelques notes sont regroupées ci-dessous.
+
+## Pins
+
+* 15 : Led R
+* 16 : Led G
+* 17 : Led B
+
+## Paquets serial
+
+Un paquet serial a la forme suivante (4 bytes) :
+
+```
+synchro (= 1) | fonction | compteur
+ 1bit         |  1 bit   |  6 bits
+```
+
+```
+synchro (= 0) | couleurR
+ 1bit         |  7 bits
+```
+
+```
+synchro (= 0) | couleurG
+ 1bit         |  7 bits
+```
+
+```
+synchro (= 0) | couleurB
+ 1bit         |  7 bits
+```
+
+## Fonctions
+
+* `0b0` : Switch immédiat
+* `0b1` : Broadcast (traite le paquet et le forward quand même avec compteur nul)
+
+## Divers
+
+* serialEvent est appelée automatiquement à la fin de loop() si des données sont dispos sur le RX.
+* Lors de l'arrivée d'un header, `serial_i = compteur`.
+* `serial_i décrit l'état
+    * `-1` => attente de header
+    * `0` => en attente des données à traiter pour la led rouge
+    * `1` => en attente des données à traiter pour la led bleue
+    * `2` => en attente des données à traiter pour la led verte
+* Pas besoin d'appel à pinMode avant un analogWrite.
