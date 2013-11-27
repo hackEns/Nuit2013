8cb1827899dac227cc5189528940a99259ee9517
hackEns <hackens@clipper.ens.fr>
2013-11-27 18:46:47 +0100

    Test serial improved

--- test/test_serial.py
+++ test/test_serial.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# -*- coding: utf8 -*-
 
 # TODO :
 # stty -hup -F /dev/ttyUSB0
@@ -6,8 +7,15 @@
 import serial
 import time
 import math
+import sys
 
-ser = serial.Serial('/dev/ttyUSB1', 115200)
+if(len(sys.argv) < 2):
+	sys.exit("Usage : ./test_serial.py PORT")
+
+try:
+	ser = serial.Serial(sys.argv[1], 115200)
+except:
+	sys.exit("Erreur à l'ouverture du port série.")
 
 for i in range(1, 127):
 	for j in [0x80, 0, 0, i]:
