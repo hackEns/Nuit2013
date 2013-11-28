908e76bc211bfd1360a831f5d59667b71d924215
Phyks <webmaster@phyks.me>
2013-11-28 13:43:57 +0100

    Code pour la Mega2560 updaté

--- Mega/Mega.ino
+++ Mega/Mega.ino
@@ -23,7 +23,7 @@ void loop() {
       if(compteur <= 12) {
         Serial1.print(incoming_byte);
       }
-      // Si compteur > 13, forward sur Serial2 (13..25)
+      // Si compteur > 12, forward sur Serial2 (13..25)
       else {
         Serial2.print((incoming_byte & B11000000) | (compteur - 13));
       }
@@ -36,11 +36,11 @@ void loop() {
     }
     // Paquet de couleur
     else {
-      // Si compteur <= 13, forward sur Serial1
+      // Si compteur <= 12, forward sur Serial1
       if(compteur <= 12) {
         Serial1.print(incoming_byte);
       }
-      // Si compteur > 13, forward sur Serial2
+      // Si compteur > 12, forward sur Serial2
       else {
         Serial2.print((incoming_byte & B11000000) | (compteur - 13));
       }
---
+++ Mega/README.md
@@ -0,0 +1,8 @@
+Code sur la Mega2560
+====
+
+La Mega2560 est située en sortie du PC de contrôle et avant le premier des ATmega de chaque couloir.
+
+Elle récupère les paquets en entrée sur le port Serial0 (USB) et les retransmet sur le port Serial1 ou Serial2, selon le couloir auquel on s'adresse.
+
+Pour plus d'infos sur les paquets serial, voir le README dans le dossier du code des ATmega.
